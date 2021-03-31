4c89d0cb3cb8e39f3c3b2e991a4e5813157e95d3
patrick-angelos <angelosp07@gmail.com>
2021-03-31 11:35:24 +0300

    Add form and button creation functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import projectModule from './project'
 import pageLoad from './page-load'
+import './styles.css';
 
 projectModule.createProject('Today')
 pageLoad(projectModule.getProjectsArray());
--- src/page-load.js
+++ src/page-load.js
@@ -1,8 +1,45 @@
+const showButton = (object, content) => {
+  const btn = document.createElement('button');
+  btn.textContent = content;
+  btn.addEventListener('click', show.bind(this, object));
+  return btn;
+}
+
+const show = (object) => {
+  object.classList.toggle('hidden');
+}
+
+const projectForm = () => {
+  const form = document.createElement('form');
+  form.classList.add('hidden');
+  const nameLabel = document.createElement('label');
+  nameLabel.setAttribute('for', 'name');
+  nameLabel.textContent = 'Name';
+
+  const name = document.createElement('input');
+  name.setAttribute('id', 'name');
+  name.type = 'text';
+
+  const submit = document.createElement('input');
+  submit.type = 'submit';
+  submit.value = 'Submit';
+
+  form.appendChild(nameLabel);
+  form.appendChild(name);
+  form.appendChild(submit);
+
+  return form;
+}
+
 const pageLoad = (projects) => {
+  let container = document.querySelector('#container');
+  const form = projectForm();
+  const projectButton = showButton(form, 'Project');
+  container.appendChild(projectButton);
+  container.appendChild(form);
   let array = projects;
   for (let i = 0; i < array.length; i++){
     let newDiv = document.createElement('div');
-    let container = document.querySelector('#container');
     newDiv.textContent = array[i].name;
     newDiv.style = 'height: 50px; width: 50px'
     container.appendChild(newDiv);
